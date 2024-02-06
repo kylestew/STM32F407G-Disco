1e509e25d6eef6ae7e2818da031ae85c9a9f0fa8
Kyle Stewart <kylestew@gmail.com>
2024-02-05 19:10:24 -0700

    Flash hello world blinky

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # STM32F407G-Disco
 Learning examples for the STM32F407G-DISC1 board from STM
+
+
+
+
+
+starting debugger:
+
+openocd -f interface/stlink.cfg -f target/stm32f4x.cfg
+
+
+
+example flashing:
+
+openocd -f interface/stlink.cfg -f target/stm32f4x.cfg -c "program apps/01-miniblink/01-miniblink.bin 0x8000000 verify reset exit"
--- apps/01-miniblink/miniblink.c
+++ apps/01-miniblink/miniblink.c
@@ -2,7 +2,7 @@
 #include <libopencm3/stm32/rcc.h>
 
 /*
- * Blinks onboard LED @ PE8
+ * Blinks onboard LED @ PD13
  */
 
 void delay(long time) {
@@ -15,14 +15,14 @@ void delay(long time) {
 static void gpio_setup(void) {
     // Enable port by enabling it's clock
     // pg 148: enable GPIO clock for port E - RCC_AHBENR: IOPE EN = 1
-    // rcc_periph_clock_enable(RCC_GPIOE);
+    rcc_periph_clock_enable(RCC_GPIOD);
 
     // Set control registers for port E pin 8
     // pg 237: general purpose output (open drain) - GPIOE MODER: MODER8 = 01
     // pg 237: set pin to push-pull - GPIOE_OTYPER: OT8 = 0
     // pg 238: set pin to high speed - GPIOE_OSSPEEDER: OSPEEDR8 = 11
     // pg 238: set pin to pull up / pull down - GPIOE_PUPDR: PUPDR8 = 00
-    // gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8);
+    gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
 }
 
 int main(void) {
@@ -30,12 +30,12 @@ int main(void) {
      * on default settings using the internal oscillator.
      */
 
-    // gpio_setup();
+    gpio_setup();
 
     // Blink the LED (PE8) on the board
     while (1) {
         // toggle LED on/off
-        // gpio_toggle(GPIOE, GPIO8);
+        gpio_toggle(GPIOD, GPIO13);
         delay(100000);
     }
 
